c99b1b7e5eda4f4619d8604eba2688df0145fe80
Eligio93 <eligio.cristantielli@gmail.com>
2024-11-12 23:26:10 +1100

    adds real link to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,26 @@
-import { useState } from "react"
-import MobileMenu from "./MobileMenu"
+import { useState } from "react";
+import MobileMenu from "./MobileMenu";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-
-
-    return (
-        <header className="relative max-w-screen-xl mx-auto">
-            <>
-                <nav>
-                    <MobileMenu />
-                    <ul className="hidden md:flex justify-center text-white gap-36 py-7 border-b">
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                    </ul>
-                </nav>
-            </>
-        </header>
-    )
+  return (
+    <header className="relative p-3">
+      <>
+        <nav className="relative">
+          <MobileMenu />
+          <ul className="hidden justify-end gap-10 pt-10 text-white lg:flex">
+            <li className="text-lg font-bold hover:text-sky-500">
+              <Link to={"/"}>Home</Link>
+            </li>
+            <li className="text-lg font-bold hover:text-pink-500">
+              <Link to="/projects">Projects </Link>
+            </li>
+            <li className="text-lg font-bold hover:text-green-500">
+              <Link to="/contacts">Contacts</Link>
+            </li>
+          </ul>
+        </nav>
+      </>
+    </header>
+  );
 }
