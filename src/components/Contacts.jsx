4d96adb68897d4ee9689db23a88a137744fbb729
Eligio93 <eligio.cristantielli@gmail.com>
2024-11-16 12:24:22 +1100

    init component

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header/Header";
+import Footer from "./Footer";
+
+export default function Contacts() {
+  return (
+    <div className="flex flex-col gap-5 rounded-lg p-3 md:p-5">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
