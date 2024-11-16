d971f9e97fea42f285d53d226b03365201348dac
Eligio93 <eligio.cristantielli@gmail.com>
2024-11-16 12:24:09 +1100

    defines projects and contacts routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Projects from "./components/Projects";
+import Contacts from "./components/Contacts";
 import App from "./App";
 import "./index.css";
 
@@ -9,9 +11,15 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
   },
-  ,{
-    path:'About'
-  }
+  ,
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/contacts",
+    element: <Contacts />,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
