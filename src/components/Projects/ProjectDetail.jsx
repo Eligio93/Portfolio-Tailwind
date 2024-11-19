573d210ad1dcda64285cfdd7f58195044ee83ad5
Eligio93 <eligio.cristantielli@gmail.com>
2024-11-19 16:06:32 +1100

    formats document and completes it with different sections

--- src/components/Projects/ProjectDetail.jsx
+++ src/components/Projects/ProjectDetail.jsx
@@ -3,12 +3,12 @@ import Footer from "../Footer";
 import Header from "../Header/Header";
 import ProjectsList from "../../data/projects.json";
 import flyingAstronaut from "/flyingAstronaut.png";
+import { Link } from "react-router-dom";
 
 export default function ProjectDetail() {
+  window.scrollTo(0, 0);
   const projectTitle = useParams().projectId;
   const project = ProjectsList.find((p) => p.title === projectTitle);
-  console.log("ENTRIES:", Object.entries(project.readMe));
-  console.log(project);
   return (
     <div className="flex flex-col gap-5 rounded-lg p-3 md:p-5">
       <Header />
@@ -43,22 +43,28 @@ export default function ProjectDetail() {
             Watch Live
           </a>
         </section>
-        <section className="flex justify-around lg:absolute lg:-left-20 lg:flex-col lg:gap-5">
-          {project["tech-stack"].map((tech) => {
-            return (
-              <div className="group sticky top-0 flex flex-col items-center justify-center gap-2 lg:flex-row lg:justify-start lg:gap-6">
-                <p className="hidden animate-fade-left text-indigo-300 animate-duration-1000 animate-fill-forwards animate-once sm:block lg:hidden lg:group-hover:block">
-                  {tech.name}
-                </p>
-                <img
-                  className="h-8 w-8 transition duration-300 sm:h-9 sm:w-9 lg:h-10 lg:w-10 lg:grayscale lg:hover:drop-shadow-md lg:hover:grayscale-0"
-                  src={tech.icon}
-                  alt={tech.name}
+        <section className="flex flex-col gap-5">
+          <h2 className="font-bold text-gray-500">Developed with</h2>
+          <div className="flex flex-wrap items-center justify-around gap-5 lg:justify-start lg:gap-8">
+            {project["tech-stack"].map((tech) => {
+              return (
+                <div
                   key={tech.name}
-                />
-              </div>
-            );
-          })}
+                  className="group flex flex-col items-center justify-center gap-2"
+                >
+                  <img
+                    className="h-8 w-8 transition duration-300 sm:h-9 sm:w-9"
+                    src={tech.icon}
+                    alt={tech.name}
+                    key={tech.name}
+                  />
+                  <p className="hidden text-indigo-300 animate-duration-1000 animate-fill-forwards animate-once group-hover:animate-fade-left sm:block lg:invisible lg:group-hover:visible">
+                    {tech.name}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
         </section>
 
         <div className="flex flex-col gap-7">
@@ -68,13 +74,31 @@ export default function ProjectDetail() {
           {Object.entries(project.readMe).map(([key, value]) => {
             return (
               <div key={key}>
-                <h2 className="text-2xl lg:text-3xl">{key}</h2>
+                <h2 className="text-2xl lg:text-3xl">{key.toUpperCase()}</h2>
                 <p className="text-md mt-5 sm:text-lg">{value}</p>
               </div>
             );
           })}
         </div>
-        <img className="mx-auto max-w-lg" src={flyingAstronaut} alt="" />
+        <hr />
+        <section className="relative flex items-center justify-center">
+          <img
+            className="max-w-52 opacity-20 lg:max-w-80"
+            src={flyingAstronaut}
+            alt=""
+          />
+          <div className="absolute flex flex-col items-center gap-10">
+            <h3 className="text-center text-3xl font-bold text-white sm:text-4xl lg:text-4xl">
+              Are you already tired of exploring?
+            </h3>
+            <Link
+              to={"/projects"}
+              className="rounded-full bg-sky-500 px-5 py-2 font-bold text-white transition hover:scale-110"
+            >
+              All Projects
+            </Link>
+          </div>
+        </section>
       </div>
       <Footer />
     </div>
