cf758f2a80fa406164348923c5bed389961b865b
Eligio93 <eligio.cristantielli@gmail.com>
2024-11-19 16:01:07 +1100

    defines future for the v7 of the react router

--- src/main.jsx
+++ src/main.jsx
@@ -7,25 +7,36 @@ import App from "./App";
 import "./index.css";
 import ProjectDetail from "./components/Projects/ProjectDetail";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+    ,
+    {
+      path: "/projects",
+      element: <Projects />,
+    },
+    {
+      path: "/contacts",
+      element: <Contacts />,
+    },
+    {
+      path: "/projects/:projectId",
+      element: <ProjectDetail />,
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
+    future: {
+      v7_relativeSplatPath: true,
+      v7_startTransition: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+    },
   },
-  ,
-  {
-    path: "/projects",
-    element: <Projects />,
-  },
-  {
-    path: "/contacts",
-    element: <Contacts />,
-  },
-  {
-    path: "/projects/:projectId",
-    element: <ProjectDetail />,
-  },
-]);
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
