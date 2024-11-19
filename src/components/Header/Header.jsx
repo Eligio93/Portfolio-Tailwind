602bf61c834214d828499d081f2748b6e4747df4
Eligio93 <eligio.cristantielli@gmail.com>
2024-11-19 16:03:27 +1100

    styles header on lg screens

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,23 +4,22 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="relative p-3">
-      <>
-        <nav className="relative">
-          <MobileMenu />
-          <ul className="hidden justify-end gap-10 pt-10 text-white lg:flex">
-            <li className="text-lg font-bold hover:text-sky-500">
-              <Link to={"/"}>Home</Link>
-            </li>
-            <li className="text-lg font-bold hover:text-pink-500">
-              <Link to="/projects">Projects </Link>
-            </li>
-            <li className="text-lg font-bold hover:text-green-500">
-              <Link to="/contacts">Contacts</Link>
-            </li>
-          </ul>
-        </nav>
-      </>
+    <header className="sticky top-0 z-50 flex items-center justify-between p-5 backdrop-blur-xl">
+      <MobileMenu />
+      <div className="hidden items-center gap-3 lg:flex"></div>
+      <nav className="relative hidden lg:flex">
+        <ul className="flex justify-end gap-10 text-white">
+          <li className="text-lg font-bold hover:text-sky-500">
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li className="text-lg font-bold hover:text-pink-500">
+            <Link to="/projects">Projects </Link>
+          </li>
+          <li className="text-lg font-bold hover:text-green-500">
+            <Link to="/contacts">Contacts</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
