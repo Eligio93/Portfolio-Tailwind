a6ffd235a85e19cf39b5366f02593ed72cd026a5
Eligio93 <eligio.cristantielli@gmail.com>
2024-11-19 16:04:04 +1100

    formats document and adds link to site routes

--- src/components/Header/MobileMenu.jsx
+++ src/components/Header/MobileMenu.jsx
@@ -1,32 +1,42 @@
-import { useState } from "react"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function MobileMenu() {
-    const [open, setOpen] = useState(false)
-    return (
-        <>
-            <button
-                className="relative w-10 h-10 text-white bg-transparent focus:outline-none md:hidden"
-                onClick={() => setOpen(!open)}>
-                <div className="absolute block w-5 transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
-                    <span
-                        aria-hidden="true"
-                        className={`block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${open ? "rotate-45" : "-translate-y-1.5"
-                            }`}></span>
-                    <span
-                        aria-hidden="true"
-                        className={`block absolute  h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${open && "opacity-0"
-                            }`}></span>
-                    <span
-                        aria-hidden="true"
-                        className={`block absolute  h-0.5 w-5 bg-current transform  transition duration-500 ease-in-out ${open ? "-rotate-45" : "translate-y-1.5"
-                            }`}></span>
-                </div>
-            </button>
-            <ul className={`${open ? "w-full absolute top-10 text-white flex flex-col items-start gap-4  bg-indigo-600 p-5 md:hidden" : "hidden"}`}>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </>
-    )
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <button
+        className="focus:outLinkne-none h-5 w-5 bg-transparent text-pink-500 lg:hidden"
+        onClick={() => setOpen(!open)}
+      >
+        <div className="w-5 transform">
+          <span
+            aria-hidden="true"
+            className={`absolute block h-0.5 w-5 transform bg-current transition duration-500 ease-in-out ${
+              open ? "rotate-45" : "-translate-y-1.5"
+            }`}
+          ></span>
+          <span
+            aria-hidden="true"
+            className={`absolute block h-0.5 w-5 transform bg-current transition duration-500 ease-in-out ${
+              open && "opacity-0"
+            }`}
+          ></span>
+          <span
+            aria-hidden="true"
+            className={`absolute block h-0.5 w-5 transform bg-current transition duration-500 ease-in-out ${
+              open ? "-rotate-45" : "translate-y-1.5"
+            }`}
+          ></span>
+        </div>
+      </button>
+      <ul
+        className={`${open ? "absolute left-0 top-10 z-[100] flex w-full animate-fade-down flex-col items-start gap-4 rounded-md bg-sky-500 p-5 text-white lg:hidden" : "hidden"}`}
+      >
+        <Link to={"/"}>Home</Link>
+        <Link to={"/projects"}>Projects</Link>
+        <Link to={"/contacts"}>Contact</Link>
+      </ul>
+    </>
+  );
 }
